947393342c325e8f175d918a3cf57ccbcdec5ac9
felipe <trc6018386@unimedbh.com.br>
2024-10-20 13:00:09 -0300

    mudando texto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Modifique o arquivo <code>src/App.js</code> e salve para ver.
         </p>
         <a
           className="App-link"
