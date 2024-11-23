0ecbeff1d1f98a286f0a0fe3b7b38cd0bb07ea9a
Felipe <contatofemachado@gmail.com>
2024-11-23 15:44:12 -0300

    teste hook 2

--- src/rotas/App.js
+++ src/rotas/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          teste - commit o <code>codigo</code> na máquina.
+          teste - novo teste commit o <code>codigo</code> na máquina.
         </p>
         <p>
           e subindo com commit.
