5c4171431bc562cf8b5c96a34fa25c97438f8c1c
Felipe <contatofemachado@gmail.com>
2024-11-23 19:22:37 -0300

    randomizando

--- src/api/jokes.js
+++ src/api/jokes.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
 const app = express();
 const port = 3003;
 
-const db = new sqlite3.Database('/home/felpsgk/Documents/react/felps-app/src/db/piadas.db');
+const db = new sqlite3.Database(
+  "/home/felpsgk/Documents/react/felps-app/src/db/piadas.db"
+);
 
-app.get('/piadas', (req, res) => {
-  db.all('SELECT * FROM piadas', [], (err, rows) => {
+app.get("/piadas", (req, res) => {
+  db.get("SELECT * FROM piadas ORDER BY RANDOM() LIMIT 1", [], (err, row) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
     res.json({
-      message: 'Successo',
-      data: rows
+      message: "Successo",
+      data: rows,
     });
   });
 });
